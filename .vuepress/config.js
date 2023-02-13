a3886e55445d34c7ba639c8483235efccc549e45
zhaomn33 <zhaomn_314@163.com>
2023-02-13 14:34:06 +0800

    config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -98,5 +98,33 @@ module.exports = {
   },
   "markdown": {
     "lineNumbers": true
-  }
+  },
+  "plugins": [
+    [
+      'cursor-effects',
+      {
+        "size": 2, // size of the particle, default: 2
+        "shape": 'circle', // ['star' | 'circle'], // shape of the particle, default: 'star'
+        "zIndex": 999999999, // z-index property of the canvas, default: 999999999
+      }
+    ],
+    [
+      '@vuepress-reco/vuepress-plugin-kan-ban-niang',
+      {
+        "theme": ['whiteCat', 'haru2', 'blackCat', 'z16'],
+        "width": 200,
+        "height": 300,
+        "messages":{
+          "welcome": '欢迎来到我的博客~',
+          "home": '心里的花，我想要带你回家。',
+          "theme": '你想要找我其他的小伙伴么',
+          "close": '一会儿再见吧～'
+        },
+        "clean": true
+      }
+    ],
+    [
+      '@vuepress-reco/vuepress-plugin-back-to-top'
+    ]
+  ]
 }
